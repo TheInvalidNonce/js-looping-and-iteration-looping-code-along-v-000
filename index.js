72b6fc4b7c06d717599d6e631d79748dca26c4e9
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-29 22:29:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function tailsNeverFails() {
   while (Math.random() >= 0.5) {
     tailsCount++;
   }
-  console.log(`You got ${tailsCount} tails in a row!`)
+  return `You got ${tailsCount} tails in a row!`
 }
